49fee0ad19685e3699918b1e4f821aca8897cb2e
Dylan Skroskznik <98244530+dskroskznik@users.noreply.github.com>
2023-12-26 06:30:57 -0500

    1480. C++ solution added

---
+++ Leetcode/Easy Problems/1480. Running Sum of 1d Array/dec-15-2022.cpp
@@ -0,0 +1,16 @@
+/*
+  December 15, 2022
+  - Runtime Achieved: 0ms
+  - Memory Achieved: 8.6 MB
+*/
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> runSum(nums.size());
+        runSum[0] = nums[0];
+        for(int s=1;s<nums.size();s++) {
+            runSum[s] = runSum[s - 1] + nums[s];
+        }
+        return runSum;
+    }
+};
