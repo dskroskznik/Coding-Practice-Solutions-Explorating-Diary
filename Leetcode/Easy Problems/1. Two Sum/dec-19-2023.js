c9bddd2af46a620c015ac678d3ed585235b8ed21
Dylan Skroskznik <98244530+dskroskznik@users.noreply.github.com>
2023-12-26 22:32:59 -0500

    1. JavaScript solution added

---
+++ Leetcode/Easy Problems/1. Two Sum/dec-19-2023.js
@@ -0,0 +1,20 @@
+/*
+  December 19, 2023
+  - Runtime Achieve: 47 ms
+  - Memory Achieve: 43.3 MB
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let dict={}
+    for (let i = 0; i < nums.length; i++) {
+        let a = target - nums[i];
+        if (a in dict) {
+            return [dict[a], i];
+        }
+        dict[nums[i]] = i;
+    }
+    return null;
+};
