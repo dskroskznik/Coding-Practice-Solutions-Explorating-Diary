e815eae2195693af24dc972d18ecab0fb812108c
Dylan Skroskznik <98244530+dskroskznik@users.noreply.github.com>
2023-12-30 04:43:21 -0500

    1758. JavaScript solution added

---
+++ Leetcode/Easy Problems/1758. Minimum Changes To Make Alternating Binary String/dec-23-2023.js
@@ -0,0 +1,23 @@
+/**
+  December 23, 2023
+  - Runtime Achieved: 0 ms 
+  - Memory Achieved: 7.5 MB
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    let len = s.length;
+    let n = 0;
+    for(let i = 0; i < len; i++) {
+        if(i % 2) {
+            if(s[i] === '1')
+                n++;
+        } 
+        else {
+            if(s[i] === '0')
+                n++;
+        }
+    }
+    return Math.min(len - n, n);
+};
